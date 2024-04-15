88dd16e7f6b1b465355e52a80b719a85436e83a8
oleven <julian.pannen@gmail.com>
2024-04-15 17:38:03 +0200

    Übung 2.2 angefangen

--- Programmieren2/Uebung2/Uebung2/Kind.java
+++ Programmieren2/Uebung2/Uebung2/Kind.java
@@ -1,11 +1,19 @@
 package Uebung2.Uebung2;
 
+import Uebung2.Uebung1.Paar;
+
 public class Kind<V, M> extends Person{
 
 
-    private <Person, Person> eltern;
+    private Paar<Person, Person> eltern;
 
     public void setEltern(Person v, Person m) {
-
+        this.eltern = new Paar<>(v, m);
     }
+
+    static boolean geschwister(Kind<Person, Person> a, Kind<Person, Person> b) {
+        return a.eltern == b.eltern;
+    }
+
+
 }
---
+++ Programmieren2/Uebung2/Uebung2/TestKind.java
@@ -0,0 +1,8 @@
+package Uebung2.Uebung2;
+
+public class TestKind {
+    public static void main(String[] args) {
+
+        Person v1 = new Person();
+    }
+}
