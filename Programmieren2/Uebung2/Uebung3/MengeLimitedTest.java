eb5acb0936ce112d68587294015861812109edd4
oleven <julian.pannen@gmail.com>
2024-04-19 14:08:19 +0200

    Übung 2 fertig

--- Programmieren2/Uebung2/Uebung3/MengeLimitedTest.java
+++ Programmieren2/Uebung2/Uebung3/MengeLimitedTest.java
@@ -1,12 +1,18 @@
 package Uebung2.Uebung3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MengeLimitedTest {
 
-    static MengeLimited<String> ml = new MengeLimited<>();
+    static MengeLimited<String> ml;
+
+    @BeforeEach
+    void init(){
+        ml = new MengeLimited<>();
+    }
 
     @Test
     void size() {
@@ -22,18 +28,34 @@ class MengeLimitedTest {
 
     @Test
     void get() {
+        ml.insert("Test");
+        assertEquals("Test", ml.get());
     }
 
     @Test
     void insert() {
-
+        ml.insert("Test");
+        assertEquals("Test", ml.get());
+        assertEquals(1, ml.size());
     }
 
     @Test
     void delete() {
+        ml.insert("Test");
+        ml.insert("Test1");
+        ml.delete("Test1");
+        assertEquals(1, ml.size());
+        assertEquals("Test", ml.get());
+
+        ml.delete("Test");
+        assertEquals(0, ml.size());
     }
 
     @Test
     void contains() {
+        ml.insert("Test");
+
+        assertTrue(ml.contains("Test"));
+        assertFalse(ml.contains("Test1"));
     }
 }
